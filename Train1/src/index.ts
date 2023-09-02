1641b7b363eda31a6d8e171bd968a4d776353686
Omar Hamadou <hamadouomar34@gmail.com>
2023-09-02 01:34:29 +0100

    interface

--- Train1/src/index.ts
+++ Train1/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello world")
+interface Person {
+    name: string;
+    lastName: string;
+    age: number;
+}
+
+const personA:Person = {
+    name: "John",
+    lastName: "Ran",
+    age: 34
+};
