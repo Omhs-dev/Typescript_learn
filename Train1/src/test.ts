20e96cac131a1267fbf6d7690d6d4f26644c42a8
Omar Hamadou <hamadouomar34@gmail.com>
2023-09-04 23:29:06 +0100

    arrays

--- Train1/src/test.ts
+++ Train1/src/test.ts
@@ -1,2 +1,21 @@
 let a: any;
 let u: unknown;
+
+a.splice();
+u.splice();
+
+
+//arrays
+const string = ['Omhs', 'typescript'];
+
+string.push('javascript');
+
+const numbers = [23, 4, 14];
+
+numbers.push(9);
+
+//mixed array
+const mixed = ['Omhs', 4, 'hello'];
+
+mixed.push('hello');
+mixed.push(6);
