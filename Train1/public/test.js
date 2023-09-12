3de17736e0097f5c389cbb565492e161de671f88
Omar Hamadou <hamadouomar34@gmail.com>
2023-09-12 02:16:49 +0100

    type aliases

--- Train1/public/test.js
+++ Train1/public/test.js
@@ -1,58 +1,7 @@
 "use strict";
 // let a: any;
 // let u: unknown;
-// a.splice();
-// // u.splice();
-// //arrays
-// const string = ['Omhs', 'typescript'];
-// string.push('javascript');
-// const numbers = [23, 4, 14];
-// numbers.push(9);
-// //mixed array
-// const mixed = ['Omhs', 4, 'hello'];
-// mixed.push('hello');
-// mixed.push(6);
-// let person = {
-//     name : "John",
-//     age : 32,
-// }
-// person.name = "Omhs";
-// person.age = 45;
-// person = {
-//     name : "Omhs",
-//     age : 34,
-// }
-// //explicit types
-// let handle: string;
-// let ages: number;
-// let presence: boolean;
-// handle = "John";
-// ages = 34;
-// presence = false;
-// // union types
-// let mixe: (string | number | boolean)[] = [];
-// mixe.push("test");
-// mixe.push(23);
-// mixe.push("hello");
-// mixe.push(false);
-// //any type
-// let surname: any = "Omhs";
-// surname = true;
-// surname = "hello";
-// surname = {agess: 23};
-//funtion basics
-// let funtionOne: Function;
-// funtionOne = () => {
-//     console.log("hello One");
-// }
-// funtionOne();
-// const functionTwo = (a: number, b: number) => {
-//     console.log(a + b);
-// }
-// functionTwo(3, 4);
-//minus operation in typescript
-const minus = (a, b) => {
-    return a - b;
+const details = (uid, item) => {
+    console.log(`${item} has a uid of ${uid}`);
 };
-let result = minus(12, 4);
-console.log(result);
+details(123, "item");
