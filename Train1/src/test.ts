419dfa3b8fd7b5fc612e1a1c4e05256a837f1d06
Omar Hamadou <hamadouomar34@gmail.com>
2023-09-16 01:35:27 +0100

    new

--- Train1/src/test.ts
+++ Train1/src/test.ts
@@ -1,61 +1,94 @@
-let a: any;
-let u: unknown;
+// let a: any;
+// let u: unknown;
 
-a.splice();
-// u.splice();
-
-
-//arrays
-const string = ['Omhs', 'typescript'];
-
-string.push('javascript');
-
-const numbers = [23, 4, 14];
-
-numbers.push(9);
-
-//mixed array
-const mixed = ['Omhs', 4, 'hello'];
-
-mixed.push('hello');
-mixed.push(6);
+// a.splice();
+// // u.splice();
 
 
+// //arrays
+// const string = ['Omhs', 'typescript'];
 
-let person = {
-    name : "John",
-    age : 32,
+// string.push('javascript');
+
+// const numbers = [23, 4, 14];
+
+// numbers.push(9);
+
+// //mixed array
+// const mixed = ['Omhs', 4, 'hello'];
+
+// mixed.push('hello');
+// mixed.push(6);
+
+
+
+// let person = {
+//     name : "John",
+//     age : 32,
+// }
+
+// person.name = "Omhs";
+// person.age = 45;
+
+// person = {
+//     name : "Omhs",
+//     age : 34,
+// }
+
+// //explicit types
+// let handle: string;
+// let ages: number;
+// let presence: boolean;
+
+// handle = "John";
+// ages = 34;
+// presence = false;
+
+
+// // union types
+// let mixe: (string | number | boolean)[] = [];
+// mixe.push("test");
+// mixe.push(23);
+// mixe.push("hello");
+// mixe.push(false);
+
+
+// //any type
+// let surname: any = "Omhs";
+
+// surname = true;
+// surname = "hello";
+// surname = {agess: 23};
+
+//funtion basics
+// let funtionOne: Function;
+
+// funtionOne = () => {
+//     console.log("hello One");
+// }
+
+// funtionOne();
+
+// const functionTwo = (a: number, b: number) => {
+//     console.log(a + b);
+// }
+
+// functionTwo(3, 4);
+//minus operation in typescript
+// const minus = (a: number, b: number) => {
+//     return a - b;
+// }
+
+// let result = minus(12, 4);
+
+// console.log(result);
+
+//type aliases
+type stringOrNum = string | number;
+
+const details = (uid: stringOrNum, item: string) => {
+    console.log(`${item} has a uid of ${uid}`);
 }
 
-person.name = "Omhs";
-person.age = 45;
-
-person = {
-    name : "Omhs",
-    age : 34,
-}
-
-//explicit types
-let handle: string;
-let ages: number;
-let presence: boolean;
-
-handle = "John";
-ages = 34;
-presence = false;
-
-
-// union types
-let mixe: (string | number | boolean)[] = [];
-mixe.push("test");
-mixe.push(23);
-mixe.push("hello");
-mixe.push(false);
-
-
-//any type
-let surname: any = "Omhs";
-
-surname = true;
-surname = "hello";
-surname = {agess: 23};
+details(123, "item");
+details(33, "holla");
